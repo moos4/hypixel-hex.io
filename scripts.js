8867f76a821c8c0645955c82af9f9d39da5c7ad8
moos4 <moos.kaas.wat@gmail.com>
2025-06-12 10:36:48 +0200

    again fixing an error

--- scripts.js
+++ scripts.js
@@ -55,6 +55,6 @@ function selectItem(item) {
 
 function loadSelectedItem(itemIndex) {
   const itemBox = document.getElementById("selected-item-box");
-  var itemData = JSON.parse(items.[itemIndex]);
+  var itemData = JSON.parse(items[itemIndex]);
   console.log(itemData.name)
 };
