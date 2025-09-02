dae926f6eb18dc1581084f8131efaf02bb27e0d9
Dradshere <6882@laurenslyceum.nl>
2025-09-02 19:03:21 +0200

    Added Enchant all groups.

--- enchant_groups.js
+++ enchant_groups.js
@@ -1,8 +1,126 @@
 const enchant_groups = `
 [
-  {  "name": "sword",
-      enchantments: [
-      "sharpness", "looting"
-      ]
+  {
+    "name": "Sword",
+    "enchantments": [
+      "Bane Of Arthropods", "Champion", "Cleave", "Critical", "Cubism", "Divine Gift", "Dragon Hunter", "Drain", "Ender Slayer", "Execute", "Experience", "Fire Aspect", "First Strike", "Giant Killer", "Impaling", "Knockback", "Lethality", "Life Steal", "Looting", "Luck", "Mana Steal", "Prosecute", "Scavenger", "Sharpness", "Smite", "Smoldering", "Tabasco", "Thunderbolt", "Thunderlord", "Titan Killer", "Triple-Strike", "Vampirism", "Venomous", "Vicious"
+    ]
   }
-]`
+]
+
+[
+  {
+    "name": "Bow",
+    "enchantments": [
+      "Chance", "Cubism", "Divine Gift", "Dragon Hunter", "Dragon Tracer", "Flame", "Impaling", "Infinite Quiver", "Overload", "Piercing", "Power", "Punch", "Smoldering", "Snipe", "Tabasco", "Vicious"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Helmet",
+    "enchantments": [
+      "Aqua Affinity", "Big Brain", "Transylvanian", "Blast Protection", "Ferocious Mana", "Fire Protection", "Growth", "Hardened Mana", "Hecatomb", "Mana Vampire", "Projectile Protection", "Protection", "Rejuvenate", "Respiration", "Respite", "Strong Mana", "Thorns", "Ice Cold", "Pesterminator"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Chestplate",
+    "enchantments": [
+      "Reflection", "Blast Protection", "Counter-Strike", "Ferocious Mana", "Fire Protection", "Growth", "Hardened Mana", "Mana Vampire", "Projectile Protection", "Protection", "Rejuvenate", "Respite", "Strong Mana", "Thorns", "Ice Cold", "Pesterminator", "True Protection"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Leggings",
+    "enchantments": [
+      "Blast Protection", "Ferocious Mana", "Fire Protection", "Growth", "Hardened Mana", "Mana Vampire", "Projectile Protection", "Protection", "Rejuvenate", "Respite", "Smarty Pants", "Strong Mana", "Thorns", "Ice Cold", "Pesterminator", "Tidal"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Boots",
+    "enchantments": [
+      "Blast Protection", "Depth Strider", "Feather Falling", "Ferocious Mana", "Fire Protection", "Frost Walker", "Growth", "Hardened Mana", "Mana Vampire", "Projectile Protection", "Protection", "Rejuvenate", "Respite", "Strong Mana", "Sugar Rush", "Thorns", "Ice Cold", "Pesterminator"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Shears",
+    "enchantments": [
+      "Efficiency", "Rainbow", "Silk Touch"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Shovel",
+    "enchantments": [
+      "Efficiency", "Silk Touch", "Smelting Touch"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Hoe",
+    "enchantments": [
+      "Cultivating", "Dedication", "Delicate", "Efficiency", "Harvesting", "Replenish", "Silk Touch", "Smelting Touch", "Turbo-Cacti", "Turbo-Cane", "Turbo-Carrot", "Turbo-Mushrooms", "Turbo-Potato", "Turbo-Warts", "Turbo-Wheat"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Axe",
+    "enchantments": [
+      "Cultivating", "Dedication", "Delicate", "Efficiency", "Replenish", "Silk Touch", "Smelting Touch", "Sunder", "Tabasco", "Turbo-Cacti", "Turbo-Cocoa", "Turbo-Melon", "Turbo-Pumpkin"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Fishing Rod",
+    "enchantments": [
+      "Angler", "Blessing", "Caster", "Charm", "Corruption", "Expertise", "Frail", "Impaling", "Looting", "Luck of the Sea", "Lure", "Magnet", "Piscary", "Spiked Hook", "Tabasco", "Fatal Tempo"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Wand",
+    "enchantments": [
+      "Ultimate Wise"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Equipment",
+    "enchantments": [
+      "Cayenne", "Green Thumb", "Prosperity"
+    ]
+  }
+]
+
+[
+  {
+    "name": "Necklace",
+    "enchantments": [
+      "Cayenne", "Green Thumb", "Prosperity", "Quantum"
+    ]
+  }
+]
