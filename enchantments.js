6293199e3a6d6c866e08e975f735837436f9d9c3
moos4 <moos.kaas.wat@gmail.com>
2025-09-02 20:44:27 +0200

    enchantments.js base

--- enchantments.js
+++ enchantments.js
@@ -1,6 +1,32 @@
 const enchantments = `
 [
   {  "name": "sharpness",
-      attack_damage_boost
+    "minecraft:attributes": [
+     {
+       "id": "block_interaction_range",
+       "amount": {
+         "type": "linear",
+         "base": 1,
+         "per_level_above_first": 1
+       },
+       "operation": "add_value"
+     }
+    ]
   }
 ]`
+
+/*
+{  "name": "any",
+    "minecraft:attributes": [
+     {
+       "id": "something",
+       "amount": { can olso just be set to a number for a constant value
+         "type": "linear", linear, levels_squared, clamped, fraction, lookup
+         "base": 1,
+         "per_level_above_first": 1
+       },
+       "operation": "any" add_value, add_multiplied_base, add_multiplied_total
+     }
+    ]
+  }
+*/
